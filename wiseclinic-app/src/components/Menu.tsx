72cc9f29cc0a0be2d82387b345659ba8a9ee6ce7
Maria Eduarda Elias Rocha <m248408@dac.unicamp.br>
2023-06-07 22:30:06 -0300

    feat: lateral menu missing logo

--- wiseclinic-app/src/components/Menu.tsx
+++ wiseclinic-app/src/components/Menu.tsx
@@ -1,16 +1,54 @@
-import { ButtonGroup, Button } from "@mui/material";
+import { Drawer, Box, List, ListItem, ListItemButton, ListItemText, createTheme, ThemeProvider } from "@mui/material";
 import React from "react";
+import '@fontsource-variable/comfortaa/';
+
+const theme = createTheme({
+    palette: {
+      background: {
+        paper: '#8D86C9',
+      },
+      text: {
+        primary: '#242038',
+        secondary: '#000',
+      },
+      action: {
+        active: '#001E3C',
+      }
+    },
+    typography:{
+        fontFamily: 'Comfortaa Variable',
+        body1: {
+            fontWeight: 800
+        }
+    }
+  });
 
 export default function Menu(){
+    const list = () => (
+        <Box
+          sx={{ width: 250, color: "text.primary" }}
+          role="presentation"
+        >
+          <List>
+            {['Listar Consultas', 'Criar Consulta', 'Cancelar Consulta'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      );
     return (
-        <React.Fragment>
-            <div className="menuStyle">
-                <ButtonGroup>
-                    <Button variant="text">Home</Button>
-                    <Button variant="text">Especialidades</Button>
-                    <Button variant="text">Entrar</Button>
-                </ButtonGroup>
-            </div>
-        </React.Fragment>
+        <ThemeProvider theme={theme}>
+        <div>
+         <React.Fragment>
+            <Drawer variant="permanent" sx={{bgcolor: "background.paper"}}>
+                {list()}
+            </Drawer>
+         </React.Fragment>
+        </div>
+        </ThemeProvider>
         )
 }
